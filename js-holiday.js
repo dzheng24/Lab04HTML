599fd801e8f8f1fdb26b328a7edb37c6aafe85fa
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-07-24 22:02:34 -0700

    holiday

---
+++ js-holiday.js
@@ -0,0 +1,14 @@
+var today = Date();
+var greeting;
+
+if (today = 4/20) {
+    greeting = 'happy 420!';
+} else if (today = 12/25) {
+    greeting = 'merry christmas!';
+} else if (today = 11/24) {
+    greeting = 'happy thanksgiving!';
+} else {
+    greeting = 'be kind';
+}
+
+document.write('<h3>' + greeting + '</h3>');
