82e2da03b12144bdff79aedbd4e3534ecb9a5b4f
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-07-24 21:43:10 -0700

    new js

---
+++ js-welcome.js
@@ -0,0 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 20) {
+    greeting = 'make sure you go to bed early on a school night!';
+} else if (hourNow > 12) {
+    greeting = 'music heals the soul';
+} else if (hourNow > 4) {
+    greeting = 'music is the language of all';
+} else {
+    greeting = 'you are still up?!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
