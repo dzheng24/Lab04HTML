85f1c9ca6e4992f93d54df9b335266ebc45784e7
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-07-26 12:13:22 -0700

    final touches

--- js-holiday.js
+++ js-holiday.js
@@ -1,26 +1,27 @@
 function sayHello() {
-    document.write('Welcome to Our Page!'}
-    return 'Welcome to Our Page';
+    document.write()
+    return '<h2> Welcome to Our Page </h2>';
+}
     
- var today = new Date();
- var m = today.getMonth()+1
- var d = today.getDate ();
- var greeting;
- var date;
- date = m + '/' + d
+ function holiday () {
+    var today = new Date();
+    var m = today.getMonth()+1
+    var d = today.getDate ();
+    var greeting;
+    var date;
+    date = m + '/' + d
 
 
- if (date === 3/8) {
-    greeting = 'happy international women day!';
- } else if (date === 12/25) {
-    greeting = 'merry christmas!';
- } else if (date === 11/24) {
-    greeting = 'happy thanksgiving!';
- } else {
-    greeting = 'be kind to one another';
- }
+    if (date === 3/8) {
+        greeting = 'happy international women day!';
+    } else if (date === 12/25) {
+        greeting = 'merry christmas!';
+    } else if (date === 11/24) {
+        greeting = 'happy thanksgiving!';
+    } else {
+        greeting = 'be kind to one another';
+    }
 
-document.write('<h3>' + greeting + '</h3>');
+    return '<h3>' + greeting + '</h3>';
 
-
-
+}
--- js-welcome.js
+++ js-welcome.js
@@ -4,7 +4,7 @@ var greeting;
 
 if (hourNow > 20) {
     greeting = 'make sure you go to bed early on a school night!';
-} else if (hourNow > 12) {
+} else if (hourNow > 11) {
     greeting = 'music heals the soul';
 } else if (hourNow > 4) {
     greeting = 'music is the language of all';
