4ad43fc9bc3d93b750e51a68be5c492c1af9852c
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-07-25 21:45:42 -0700

    single quotes

--- js-holiday.js
+++ js-holiday.js
@@ -1,16 +1,16 @@
 var today = new Date();
 var m = today.getMonth()+1
-var d= today.getDate ();
+var d = today.getDate ();
 var greeting;
 var date;
 date = m + '/' + d
 
 
-if (today = 4/20) {
-    greeting = 'happy 420!';
-} else if (today = 12/25) {
+if (today = '3/8') {
+    greeting = 'happy international women day!';
+} else if (today = '12/25') {
     greeting = 'merry christmas!';
-} else if (today = 11/24) {
+} else if (today = '11/24') {
     greeting = 'happy thanksgiving!';
 } else {
     greeting = 'be kind';
